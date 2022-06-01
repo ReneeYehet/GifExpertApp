f911caed58dadac72106f6a4e0a933be3c662710
ReneeYehet <ReneeEspinoza3@gmail.com>
2022-05-31 20:37:39 -0700

    useEffect
    useEffect hace que podamos condicionar, la renderizacion de un componente. En este caso al tocar el boton que, como hace un cambio en el state, con el useEffect solo permite renderizar una vez

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+//useEffect permite poder ejecutar codigo de manera condicional
 
 export const GifGrid = ({category}) => {
+
+    const [ count, setCount ] = useState(0);
+
+    //Lo que significa que usando useEffect hace que este componente solo sea renderizado por primera vez
+    useEffect( () => {
+        getGifs();
+    }, []); //[Lista de dependencias] -> si la dejamos vacia, useEffect solo se va ejecutar una vez
 
     const getGifs = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=mVV3dLufqIKZlgCGXO2QIMTXUrIGmVBd';
@@ -18,12 +27,11 @@ export const GifGrid = ({category}) => {
         console.log(gifs);
     }
 
-    getGifs();
     return (
         <div>
-            <h3>
-                {category}
-            </h3>
+            <h3>{category}</h3>
+            <h4>{count}</h4>
+            <button onClick={() => setCount(count+1)}></button>
         </div>
     )
 }
